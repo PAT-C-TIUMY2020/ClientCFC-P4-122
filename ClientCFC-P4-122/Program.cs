cfbc733feaeb416be4db5708ae360f9cc5a03bb5
Habib Santoso <habib.santoso.ft18@mail.umy.ac.id>
2020-10-23 18:43:34 +0700

    Add project files.

---
+++ ClientCFC-P4-122/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using ServiceMtk_P2_20180140122;
+
+namespace ClientCFC_P4_122
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BasicHttpBinding bind = new BasicHttpBinding();
+            ChannelFactory<IMatematika> objChannel = new
+           ChannelFactory<IMatematika>(bind, "http://localhost:1919");
+            IMatematika obj = objChannel.CreateChannel();
+            double hasilTambah = obj.Tambah(1, 2);
+            Console.WriteLine("1 + 2 = " + hasilTambah);
+            double hasilKurang = obj.Kurang(3, 2);
+            Console.WriteLine("3 - 2 = " + hasilKurang);
+            double hasilKali = obj.Kali(2, 2);
+            Console.WriteLine("2 x 2 = " + hasilKali);
+            double hasilBagi = obj.Bagi(2, 2);
+            Console.WriteLine("2 / 2 = " + hasilBagi);
+            Console.ReadLine();
+            Koordinat objk = new Koordinat();
+
+        }
+    }
+}
